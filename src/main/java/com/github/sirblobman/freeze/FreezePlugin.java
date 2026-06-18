659261864434486ee99e0465917f9e4ab51067a6
SirBlobman <blobmansrealm@gmail.com>
2026-06-18 17:31:06 -0400

    Reset classes

--- src/main/java/com/github/sirblobman/freeze/FreezePlugin.java
+++ src/main/java/com/github/sirblobman/freeze/FreezePlugin.java
@@ -1,164 +1,20 @@
 package com.github.sirblobman.freeze;
 
-import java.lang.reflect.Method;
-import java.util.Collection;
-
-import org.jetbrains.annotations.NotNull;
-
-import org.bukkit.Bukkit;
-import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.github.sirblobman.api.configuration.ConfigurationManager;
-import com.github.sirblobman.api.core.CorePlugin;
-import com.github.sirblobman.api.language.Language;
-import com.github.sirblobman.api.language.LanguageManager;
-import com.github.sirblobman.api.plugin.ConfigurablePlugin;
-import com.github.sirblobman.api.update.SpigotUpdateManager;
-import com.github.sirblobman.freeze.command.CommandFreeze;
-import com.github.sirblobman.freeze.command.CommandFreezeAll;
-import com.github.sirblobman.freeze.command.CommandFreezeReload;
-import com.github.sirblobman.freeze.command.CommandMelt;
-import com.github.sirblobman.freeze.command.CommandMeltAll;
-import com.github.sirblobman.freeze.configuration.FreezeConfiguration;
-import com.github.sirblobman.freeze.listener.ListenerBreakAndPlace;
-import com.github.sirblobman.freeze.listener.ListenerCommand;
-import com.github.sirblobman.freeze.listener.ListenerDamage;
-import com.github.sirblobman.freeze.listener.ListenerFakeMenu;
-import com.github.sirblobman.freeze.listener.ListenerItemDropping;
-import com.github.sirblobman.freeze.listener.ListenerItemMoving;
-import com.github.sirblobman.freeze.listener.ListenerMove;
-import com.github.sirblobman.freeze.listener.ListenerPowderedSnowEffect;
-import com.github.sirblobman.freeze.listener.ListenerTeleport;
-import com.github.sirblobman.freeze.manager.FreezeManager;
-import com.github.sirblobman.freeze.menu.FakeMenu;
-import com.github.sirblobman.freeze.task.MeltTask;
-import com.github.sirblobman.api.shaded.bstats.bukkit.Metrics;
-import com.github.sirblobman.api.shaded.bstats.charts.SimplePie;
-
-public final class FreezePlugin extends ConfigurablePlugin {
-    private final FreezeManager freezeManager;
-    private final FreezeConfiguration configuration;
-
-    public FreezePlugin() {
-        this.freezeManager = new FreezeManager(this);
-        this.configuration = new FreezeConfiguration(this);
-    }
-
+public final class FreezePlugin extends JavaPlugin {
     @Override
     public void onLoad() {
-        ConfigurationManager configurationManager = getConfigurationManager();
-        configurationManager.saveDefault("config.yml");
-
-        LanguageManager languageManager = getLanguageManager();
-        languageManager.saveDefaultLanguageFiles();
+        // TODO
     }
 
     @Override
     public void onEnable() {
-        reloadConfiguration();
-
-        LanguageManager languageManager = getLanguageManager();
-        languageManager.onPluginEnable();
-
-        registerCommands();
-        registerListeners();
-        registerTasks();
-
-        registerUpdateChecker();
-        register_bStats();
+        // TODO
     }
 
     @Override
     public void onDisable() {
-        Collection<? extends Player> playerCollection = Bukkit.getOnlinePlayers();
-        for (Player player : playerCollection) {
-            closeFakeMenu(player);
-        }
-    }
-
-    @Override
-    protected void reloadConfiguration() {
-        ConfigurationManager configurationManager = getConfigurationManager();
-        configurationManager.reload("config.yml");
-
-        YamlConfiguration configurationFile = configurationManager.get("config.yml");
-        FreezeConfiguration configuration = getConfiguration();
-        configuration.load(configurationFile);
-
-        LanguageManager languageManager = getLanguageManager();
-        languageManager.reloadLanguages();
-    }
-
-    public void closeFakeMenu(Player player) {
-        try {
-            Class<?> class_InventoryView = Class.forName("org.bukkit.inventory.InventoryView");
-            Method method_getTopInventory = class_InventoryView.getMethod("getTopInventory");
-
-            Object openInventory = player.getOpenInventory();
-            Inventory topInventory = (Inventory) method_getTopInventory.invoke(openInventory);
-            if (topInventory == null) {
-                return;
-            }
-
-            InventoryHolder holder = topInventory.getHolder();
-            if (holder instanceof FakeMenu) {
-                player.closeInventory();
-            }
-        } catch (ReflectiveOperationException ex) {
-            throw new IllegalStateException("Unexpected Reflection Error:", ex);
-        }
-    }
-
-    public @NotNull FreezeManager getFreezeManager() {
-        return this.freezeManager;
-    }
-
-    public @NotNull FreezeConfiguration getConfiguration() {
-        return this.configuration;
-    }
-
-    private void registerCommands() {
-        new CommandFreeze(this).register();
-        new CommandFreezeAll(this).register();
-        new CommandFreezeReload(this).register();
-        new CommandMelt(this).register();
-        new CommandMeltAll(this).register();
-    }
-
-    private void registerListeners() {
-        new ListenerBreakAndPlace(this).register();
-        new ListenerCommand(this).register();
-        new ListenerDamage(this).register();
-        new ListenerMove(this).register();
-        new ListenerTeleport(this).register();
-        new ListenerFakeMenu(this).register();
-        new ListenerItemDropping(this).register();
-        new ListenerItemMoving(this).register();
-        new ListenerPowderedSnowEffect(this).register();
-    }
-
-    private void registerTasks() {
-        new MeltTask(this).register();
-    }
-
-    private void registerUpdateChecker() {
-        CorePlugin corePlugin = JavaPlugin.getPlugin(CorePlugin.class);
-        SpigotUpdateManager updateManager = corePlugin.getSpigotUpdateManager();
-        updateManager.addResource(this, 31822L);
-    }
-
-    private void register_bStats() {
-        Metrics metrics = new Metrics(this, 16174);
-        metrics.addCustomChart(new SimplePie("selected_language", this::getDefaultLanguageCode));
-    }
-
-    private @NotNull String getDefaultLanguageCode() {
-        LanguageManager languageManager = getLanguageManager();
-        Language defaultLanguage = languageManager.getDefaultLanguage();
-        return (defaultLanguage == null ? "none" : defaultLanguage.getLanguageName());
+        // TODO
     }
 }
